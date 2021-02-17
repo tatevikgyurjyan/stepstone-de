91830a90a942b7cad4b7d30912962ecada7b62bb
tatevikgyurjyan <tatevik.gyurjyan@gmail.com>
2021-02-17 23:38:20 +0100

    Comments are added to test

--- src/test/java/am/itu/qa/stepstone/aboutus/page/test/StepStoneAboutTest.java
+++ src/test/java/am/itu/qa/stepstone/aboutus/page/test/StepStoneAboutTest.java
@@ -15,46 +15,46 @@ public class StepStoneAboutTest extends StepStoneBaseTest{
 
 	@Test
 	public void readAboutInfo() throws InterruptedException {
-		System.out.println("First of all ACCEPTS COOKIES");
+		System.out.println("--1--First of all ACCEPTS COOKIES");
 		StepStoneHomePage homePage = new StepStoneHomePage(driver);
 		homePage.acceptCookies();
 		Thread.sleep(2000);
 
-		System.out.println("Then clicks on 'WHO IS STEPSTONE' button to read about the mission of the company");
+		System.out.println("--2--Then clicks on 'WHO IS STEPSTONE' button to read about the mission of the company");
 		StepStoneAboutPage aboutPage = homePage.clickOnWhoIsStepstone();
 		Thread.sleep(4000);
 		
-		System.out.println("Dismiss thrown cookies to be able to read timeline of the company");
+		System.out.println("--3--Dismiss thrown cookies to be able to read timeline of the company");
 		aboutPage.dismissCookies();
 		StepStoneAboutTimeLinePage timeLinePage = aboutPage.clickOnOurHistory();
 		Thread.sleep(5000);
 		
-		System.out.println("Clicks on 'Management' link to read about managing staff");
+		System.out.println("--4--Clicks on 'Management' link to read about managing staff");
 		StepStoneManagementPage management = timeLinePage.clickOnManagementLink();
 		Thread.sleep(3000);
 
-		System.out.println("Then clicks on CEO to read about more information");
+		System.out.println("--5--Then clicks on CEO to read about more information");
 		StepStoneCEOPage ceo = management.viewCEOPage();
 		Thread.sleep(2000);
 		
-		System.out.println("After checking CEO's position clicks on 'Next' btn to read about CFO and then goes again back to CEO");
+		System.out.println("--6--After checking CEO's position clicks on 'Next' btn to read about CFO and then goes again back to CEO");
 		StepStoneCFOPage cfo = ceo.clickNextBtn();
 		Thread.sleep(2000);
 		cfo.backToList();
 		
-		System.out.println("Then goes to 'Contact' to check how to connect with");
+		System.out.println("--7--Then goes to 'Contact' to check how to connect with");
 		StepStoneContactsPage contactsPage = management.clickOnContactLink();
 		Thread.sleep(3000);
 		
-		System.out.println("Clicks on 'Finding Job' to receive notifications about the appropriate jobe");
+		System.out.println("--8--Clicks on 'Finding Job' to receive notifications about the appropriate jobe");
 		contactsPage.chooseFindingJobToReceiveMoreInformation();
 		Thread.sleep(3000);
 		
-		System.out.println("Types in textarea about the necessary help");
+		System.out.println("--9--Types in textarea about the necessary help");
 		contactsPage.sendDetailsAboutPreferences("I would like to receive more appropriate information");
 		Thread.sleep(8000);
 		
-		System.out.println("Clicks on 'Send request' button");
+		System.out.println("--10--Clicks on 'Send request' button");
 		contactsPage.sendRequest();
 	}
 	
